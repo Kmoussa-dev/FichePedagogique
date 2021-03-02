98f44aa3e2f94b085c5c797e22c4ea868a117559
o2165471 <pierre.mvondo@etu.univ-orleans.fr>
2021-03-02 19:25:51 +0100

    modification des classes etudiantType et ModuleType

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Etudiant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +15,40 @@ class EtudiantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('numeroEtu')
+            ->add('numeroEtu', NumberType::class, array(
+                'required' => true,
+                'attr' => [
+                    'type' => 'text',
+                    'class' => 'form-control input-inline datepicker',
+                    'data-provide' => 'numberpicker',
+                ],
+                'html5' => true,
+            ))
             ->add('nom')
             ->add('prenom')
             ->add('email')
-            ->add('dateNaissance')
-            ->add('tel')
+            ->add('dateNaissance', DateType::class, array(
+                'required' => true,
+                'widget' => 'single_text',
+                'attr' => [
+                    'type' => 'text',
+                    'class' => 'form-control input-inline datepicker',
+                    'data-provide' => 'datepicker',
+                ],
+                'html5' => true,
+            ))
+            ->add('tel', NumberType::class, array(
+                'required' => true,
+                'attr' => [
+                    'type' => 'text',
+                    'class' => 'form-control input-inline datepicker',
+                    'data-provide' => 'numberpicker',
+
+                ],
+                'html5' => true,
+            ))
             ->add('adresse')
+
         ;
     }
 
